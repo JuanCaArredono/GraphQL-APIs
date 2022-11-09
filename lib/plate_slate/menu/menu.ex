4d634030d512221635d3982150de23f0f65d7832
JuanCaArredono <juan.arredondo@diverza.com>
2022-11-09 20:28:04 +0000

    add a test for querys

--- lib/plate_slate/menu/menu.ex
+++ lib/plate_slate/menu/menu.ex
@@ -112,8 +112,8 @@ defmodule PlateSlate.Menu do
 
   alias PlateSlate.Menu.Item
 
-  @doc """
-  Returns the list of items.
+   @doc """
+  Returns the list of items
 
   ## Examples
 
@@ -121,8 +121,39 @@ defmodule PlateSlate.Menu do
       [%Item{}, ...]
 
   """
-  def list_items do
-    Repo.all(Item)
+  def list_items(args) do
+    IO.puts "These are our arguments: #{inspect(args)}"
+    args
+    |> Enum.reduce(Item, fn
+      {:order, order}, query ->
+        query |> order_by({^order, :name})
+      {:filter, filter}, query ->
+        query |> filter_with(filter)
+    end)
+    |> Repo.all
+  end
+
+  defp filter_with(query, filter) do
+    Enum.reduce(filter, query, fn
+      {:name, name}, query ->
+        from q in query, where: ilike(q.name, ^"%#{name}%")
+      {:priced_above, price}, query ->
+        from q in query, where: q.price >= ^price
+      {:priced_below, price}, query ->
+        from q in query, where: q.price <= ^price
+      {:added_after, date}, query ->
+        from q in query, where: q.added_on >= ^date
+      {:added_before, date}, query ->
+        from q in query, where: q.added_on <= ^date
+      {:category, category_name}, query ->
+        from q in query,
+          join: c in assoc(q, :category),
+          where: ilike(c.name, ^"%#{category_name}%")
+      {:tag, tag_name}, query ->
+        from q in query,
+          join: t in assoc(q, :tags),
+          where: ilike(t.name, ^"%#{tag_name}%")
+    end)
   end
 
   @doc """
